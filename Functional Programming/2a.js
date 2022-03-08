e81e3f1651c32e4f20fe9e01420116ec171ea850
shivanigangadharan <shivani.gangadharan@gmail.com>
2022-03-08 12:29:21 +0530

    added Q.2.a

--- Functional Programming/2a.js
+++ Functional Programming/2a.js
@@ -1,4 +1,14 @@
 //Q.1. Find the number of strings with similar number of characters. Return in the form of object with key as length of string and value as no of strings with that length
 
-//doubt.
+const arr = ["apple", "mango", "shivani", "tanay", "four"];
 
+const myFunc = (accObj, curr) => {
+    if (accObj[curr.length] == undefined) {
+        console.log("Adding new : ", curr.length);
+        return { ...accObj, [curr.length]: 1 };
+    } else {
+        return { ...accObj, [curr.length]: accObj[curr.length] + 1 };
+    }
+};
+
+console.log(arr.reduce(myFunc, {}));
